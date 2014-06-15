9ace166adbb06553772df5885aa9e18a680a4966
王 瀚宇 <oukanu@oumato-MacBook-Pro.local>
2014-06-15 23:59:12 +0800

    add android and unity project for FPS

---
+++ GlassGame_FPS_Unity/Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+	public Material cubeMaterial;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * 1 * Time.deltaTime;
+
+		//touch
+		handleTouch ();
+	}
+
+	void handleTouch(){
+
+		if (AndroidInput.touchCountSecondary == 1) {
+			
+			Touch touch = AndroidInput.GetSecondaryTouch(0);
+			switch (touch.phase) { //following are 2 cases
+				
+			case TouchPhase.Began: //here begins the 1st case
+				cubeMaterial.color = new Color((cubeMaterial.color.r+0.1f)%1.0f,(cubeMaterial.color.g+0.1f)%1.0f,(cubeMaterial.color.b+0.1f)%1.0f);
+
+
+				Debug.Log ("Touch!");
+				break; //here ends the 1st case
+				
+			case TouchPhase.Ended: //here begins the 2nd case
+				break;
+			}
+		}
+	}
+}
---
+++ GlassGame_FPS_Unity/Assets/GyroCamera.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroCamera : MonoBehaviour {
+	Vector3 initialDirection;
+	bool setInitialDirection;
+	Vector3 identityAngle;
+	// Use this for initialization
+	void Start () {
+
+		//init 
+		setInitialDirection = false;
+		identityAngle = new Vector3 (0, 0, 0);
+
+		//check enable
+		bool gyoBool = SystemInfo.supportsGyroscope;
+		//enable gtro
+		if( gyoBool ) {
+			Input.gyro.enabled = true;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//update direction
+		updateDirectionFromGyro ();
+	}
+
+	void updateDirectionFromGyro(){
+		Debug.Log ("Gyro:"+Input.gyro.attitude);
+
+		//get initial Rotation
+		if((setInitialDirection == false) && (Input.gyro.attitude.eulerAngles != identityAngle)){
+			//initial
+			initialDirection = Input.gyro.attitude.eulerAngles;
+			setInitialDirection = true;
+		}
+
+		//calc rotate angle
+		Vector3 rotateAngle = Input.gyro.attitude.eulerAngles - initialDirection;
+
+		//set camera rotation
+		//transform.rotation = ConvertRotation(Quaternion.Euler(rotateAngle))* GetRotFix();
+		//transform.rotation = ConvertRotation(Input.gyro.attitude)* GetRotFix();
+		transform.rotation = Quaternion.Euler(rotateAngle);
+
+		//debug
+		Debug.Log ("Initial:"+initialDirection);
+		Debug.Log ("Rotation:"+transform.rotation.eulerAngles);
+
+	}
+	
+	private static Quaternion ConvertRotation(Quaternion q)
+	{
+		return new Quaternion(q.x, q.y, -q.z, -q.w);
+	}
+	
+	private Quaternion GetRotFix()
+	{
+		if (Screen.orientation == ScreenOrientation.Portrait)
+			return Quaternion.identity;
+		if (Screen.orientation == ScreenOrientation.LandscapeLeft
+		    || Screen.orientation == ScreenOrientation.Landscape)
+			return Quaternion.Euler(0, 0, -90);
+		if (Screen.orientation == ScreenOrientation.LandscapeRight)
+			return Quaternion.Euler(0, 0, 90);
+		if (Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+			return Quaternion.Euler(0, 0, 180);
+		return Quaternion.identity;
+	}
+}
