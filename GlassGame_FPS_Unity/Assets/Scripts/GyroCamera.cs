51517bac8b49845ec1e68c4c6d30111d245ee8bc
王瀚宇 <huw12313212@gmail.com>
2014-06-18 22:32:43 +0800

    +gyro calibration

--- GlassGame_FPS_Unity/Assets/Scripts/GyroCamera.cs
+++ GlassGame_FPS_Unity/Assets/Scripts/GyroCamera.cs
@@ -2,30 +2,18 @@
 using System.Collections;
 
 public class GyroCamera : MonoBehaviour {
-	Vector3 initialDirection;
-	bool setInitialDirection;
-	Vector3 identityAngle;
-
 
 	private bool gyroBool ;
 	private Quaternion rotFix ;
 	private Gyroscope gyro;
 
 
+	private bool isInitY =false;
+	private float initY = 0;
+
+
 	// Use this for initialization
 	void Start () {
-
-		/*
-		//init 
-		setInitialDirection = false;
-		identityAngle = new Vector3 (0, 0, 0);
-
-		//check enable
-		bool gyoBool = SystemInfo.supportsGyroscope;
-		//enable gtro
-		if( gyoBool ) {
-			Input.gyro.enabled = true;
-		}*/
 
 		Transform originalParent = transform.parent; // check if this transform has a parent
 		GameObject camParent = new GameObject ("camParent"); // make a new parent
@@ -55,55 +43,23 @@ public class GyroCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//update direction
-		//updateDirectionFromGyro ();
 
 		if (gyroBool) {
-			Quaternion camRot = gyro.attitude * rotFix;
+
+
+			if (!isInitY) 
+			{
+				initY = gyro.attitude.y;
+				isInitY = true;
+				Debug.Log("initY:"+initY);
+			}
+
+			Vector3 faceVector = gyro.attitude.eulerAngles;
+			faceVector.y -= initY;
+
+			Quaternion camRot = Quaternion.Euler(faceVector) * rotFix;
 			transform.localRotation = camRot;
 		}
 	}
-
-	void updateDirectionFromGyro(){
-		Debug.Log ("Gyro:"+Input.gyro.attitude);
-
-		//get initial Rotation
-		if((setInitialDirection == false) && (Input.gyro.attitude.eulerAngles != identityAngle)){
-			//initial
-			initialDirection = Input.gyro.attitude.eulerAngles;
-			setInitialDirection = true;
-		}
-
-		//calc rotate angle
-		Vector3 rotateAngle = Input.gyro.attitude.eulerAngles - initialDirection;
-
-		//set camera rotation
-		//transform.rotation = ConvertRotation(Quaternion.Euler(rotateAngle))* GetRotFix();
-		//transform.rotation = ConvertRotation(Input.gyro.attitude)* GetRotFix();
-		transform.rotation = Quaternion.Euler(rotateAngle);
-
-		//debug
-		Debug.Log ("Initial:"+initialDirection);
-		Debug.Log ("Rotation:"+transform.rotation.eulerAngles);
-
-	}
 	
-	private static Quaternion ConvertRotation(Quaternion q)
-	{
-		return new Quaternion(q.x, q.y, -q.z, -q.w);
-	}
-	
-	private Quaternion GetRotFix()
-	{
-		if (Screen.orientation == ScreenOrientation.Portrait)
-			return Quaternion.identity;
-		if (Screen.orientation == ScreenOrientation.LandscapeLeft
-		    || Screen.orientation == ScreenOrientation.Landscape)
-			return Quaternion.Euler(0, 0, -90);
-		if (Screen.orientation == ScreenOrientation.LandscapeRight)
-			return Quaternion.Euler(0, 0, 90);
-		if (Screen.orientation == ScreenOrientation.PortraitUpsideDown)
-			return Quaternion.Euler(0, 0, 180);
-		return Quaternion.identity;
-	}
 }
